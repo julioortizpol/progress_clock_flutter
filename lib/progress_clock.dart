7721e41c71da0bbb61d2bb50b3534352a32323f9
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-01-12 18:43:11 -0400

    add clock dependencies and clock logic

---
+++ lib/progress_clock.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_helper/model.dart';
+
+import 'package:vector_math/vector_math_64.dart' show radians;
+
+final radiansPerTick = radians(360 / 60);
+final radiansPerHour = radians(360 / 12);
+
+class ProgressClock extends StatefulWidget {
+  const ProgressClock(this.model);
+
+  final ClockModel model;
+
+  @override
+  _ProgressClockState createState() => _ProgressClockState();
+}
+
+class _ProgressClockState extends State<ProgressClock> {
+  var _now = DateTime.now();
+  var _temperature = '';
+  var _temperatureRange = '';
+  var _condition = '';
+  var _location = '';
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.model.addListener(_updateModel);
+    // Set the initial values.
+    _updateTime();
+    _updateModel();
+  }
+
+  @override
+  void didUpdateWidget(ProgressClock oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.model != oldWidget.model) {
+      oldWidget.model.removeListener(_updateModel);
+      widget.model.addListener(_updateModel);
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    widget.model.removeListener(_updateModel);
+    super.dispose();
+  }
+
+  void _updateModel() {
+    setState(() {
+      _temperature = widget.model.temperatureString;
+      _temperatureRange = '(${widget.model.low} - ${widget.model.highString})';
+      _condition = widget.model.weatherString;
+      _location = widget.model.location;
+    });
+  }
+
+  void _updateTime() {
+    setState(() {
+      _now = DateTime.now();
+      // Update once per second. Make sure to do it at the beginning of each
+      // new second, so that the clock is accurate.
+      _timer = Timer(
+        Duration(seconds: 1) - Duration(milliseconds: _now.millisecond),
+        _updateTime,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      "${_now.hour} : ${_now.minute} : ${_now.second}",
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+    ));
+  }
+}
